b96ea1180e10b6c265d6fbd57e2e6a08ab6bf32b
Daniele <danieledetommaso1995@gmail.com>
2020-05-22 11:15:29 +0200

    Validazione variabili

--- js/script.js
+++ js/script.js
@@ -5,16 +5,26 @@ var eta = document.getElementById('form-eta');
 var genera = document.getElementById('genera');
 var reset = document.getElementById('reset');
 var biglietto = document.getElementById('biglietto');
+var errore = document.getElementById('errore');
+var errorekm = document.getElementById('errorekm');
 
 // Impostazioni bottone genera
- genera.addEventListener('click',
- function() {
+genera.addEventListener('click',
+function() {
 
   // Dichiarazione valore variabili
   var etaValue = document.getElementById('form-eta').value;
   var kmValue = document.getElementById('form-km').value;
+  kmValue = parseInt(kmValue);
   var prezzo = kmValue * 0.21;
   var sconto;
+
+  // Variabil biglietto
+  var prezzoBigl = document.getElementById('bigl-costo');
+  var nomeBigl = document.getElementById('bigl-nome');
+  var offertaBigl = document.getElementById('bigl-offerta')
+  var carrozzaBigl = document.getElementById('bigl-carrozza');
+  var codiceBigl = document.getElementById('bigl-codice');
 
   // Calcolo sconto
   if ( (etaValue != 'minorenne') && (etaValue != 'over')) {
@@ -29,11 +39,42 @@ var biglietto = document.getElementById('biglietto');
 
   // Stampa informazioni su biglietto
   biglietto.className = biglietto.classList + ' visible';
-  document.getElementById('bigl-nome').innerHTML = nome.value;
-  document.getElementById('bigl-offerta').innerHTML = sconto;
-  document.getElementById('bigl-carrozza').innerHTML = Math.floor((Math.random() * 10)+1);
-  document.getElementById('bigl-codice').innerHTML = Math.floor(Math.random() * 10000);
-  document.getElementById('bigl-costo').innerHTML = prezzo.toFixed(2);
+
+  // Validazione nome
+  if ( isNaN(nome.value) ) {
+    nomeBigl.innerHTML = nome.value;
+    errore.className = ' hidden';
+  } else {
+    nomeBigl.innerHTML = 'Errore';
+    errore.className = ' visible';
+  }
+
+  // Validazione prezzo
+  if ( kmValue < 1 ) {
+    prezzoBigl.innerHTML = 'Errore';
+    errorekm.className = ' visible';
+  } else if (kmValue) {
+    prezzoBigl.innerHTML = prezzo.toFixed(2) + ' €';
+    prezzoBigl.className = prezzoBigl.classList + ' green';
+    errorekm.className = ' hidden';
+  } else {
+    prezzoBigl.innerHTML = 'Errore';
+    errorekm.className = ' visible';
+  }
+
+  // Se esiste un errore mandare messaggio a tutti
+  if (( !(isNaN(nome.value)) ) || ( isNaN(prezzo) )) {
+    offertaBigl.innerHTML = 'Errore';
+    carrozzaBigl.innerHTML = 'Errore';
+    codiceBigl.innerHTML = 'Errore';
+    prezzoBigl.innerHTML = 'Errore';
+    nomeBigl.innerHTML = 'Errore';
+  } else {
+    offertaBigl.innerHTML = sconto;
+    carrozzaBigl.innerHTML = Math.floor((Math.random() * 10)+1);
+    codiceBigl.innerHTML = Math.floor(Math.random() * 10000);
+  }
+
  }
 );
 
@@ -42,11 +83,13 @@ reset.addEventListener('click',
 function() {
   nome.value = '';
   km.value = '';
-  eta.value = 'maggiorenne'
-  document.getElementById('bigl-nome').innerHTML = '';
-  document.getElementById('bigl-offerta').innerHTML = '';
-  document.getElementById('bigl-carrozza').innerHTML = '';
-  document.getElementById('bigl-codice').innerHTML = '';
-  document.getElementById('bigl-costo').innerHTML = '';
+  prezzoBigl.innerHTML = '';
+  nomeBigl.innerHTML = '';
+  offertaBigl.innerHTML = '';
+  carrozzaBigl.innerHTML = '';
+  codiceBigl.innerHTML = '';
+  eta.value = 'maggiorenne';
+  errore.className = 'hidden';
+  errorekm.className = 'hidden';
 }
 );
